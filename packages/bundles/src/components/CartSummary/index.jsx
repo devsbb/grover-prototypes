1f67ff3189d20028b6657241954ef7854bbd936f
stephanLeece <stephan.leece@outlook.com>
2020-06-24 14:48:34 +0200

    styling tweaks

--- packages/bundles/src/components/CartSummary/index.jsx
+++ packages/bundles/src/components/CartSummary/index.jsx
@@ -2,23 +2,24 @@ import React, { useContext } from "react";
 
 import { CartContext } from "../../CartContext/CartContext.js";
 
-import { Wrap, Title, PlanId, PlanSelector, Plan, Cta } from "./styles";
+import { Wrap, Title, ProductsWrap } from "./styles";
 
 export default () => {
   const { cart } = useContext(CartContext);
-  console.log('cart', cart)
   return (
     <Wrap>
       <Title>Your Cart: </Title>
-      {!cart.length && <p>is empty</p>}
-      {!!cart.length &&
-        cart.map((product) => (
-          <div>
-            <p>Product ID: {product.title}</p>
-            <p>Product ID: {product.id}</p>
-            <p>Plan Legnth: {product.planLength}</p>
-          </div>
-        ))}
+      {!cart.length && <p>...is empty</p>}
+      <ProductsWrap>
+        {!!cart.length &&
+          cart.map((product) => (
+            <div>
+              <p>Product Name: {product.title}</p>
+              <p>Product ID: {product.id}</p>
+              <p>Plan Length: {product.planLength}</p>
+            </div>
+          ))}
+        </ProductsWrap>
     </Wrap>
   );
 };
--- packages/bundles/src/components/CartSummary/styles.js
+++ packages/bundles/src/components/CartSummary/styles.js
@@ -4,28 +4,21 @@ export const Wrap = nano('div')({
     background: 'white',
     border: '1px solid black',
     borderRadius: 8,
-    padding: 32,
+    paddingTop: 16,
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center',
     alignItems: 'center',
+    width: 400,
+    height: 400,
 })
 
 export const Title = nano('h3')({
     marginBottom: 16,
 })
 
-export const PlanId = nano('p')({
-    marginBottom: 16,
+export const ProductsWrap = nano('div')({
+    maxHeight: '100%',
+    overflow: 'scroll',
+    padding: '0px 16px',
+    maxHeight: '100%',
 })
-
-export const PlanSelector = nano('div')({
-    display: 'flex',
-    marginBottom: 32,
-})
-
-export const Plan = nano('p')({
-    margin: '0px 16px',
-})
-
-export const Cta = nano('button')({})
